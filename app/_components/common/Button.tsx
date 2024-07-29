73eb001f47d4531b36ee3db6d9ea8185878ef164
jmj <wjsdncl2222@gmail.com>
2024-07-29 15:32:18 +0900

    ✨ Feat: Button 컴포넌트 버튼 크기 고정 - globals.css에 버튼 스타일 제거

--- app/_components/common/Button.tsx
+++ app/_components/common/Button.tsx
@@ -3,12 +3,10 @@
 
 "use client";
 
-import classNames from "classnames";
 import Link from "next/link";
 import { MouseEvent, PropsWithChildren } from "react";
 
 interface ButtonProps extends PropsWithChildren {
-	className?: string;
 	variant?: "primary" | "secondary" | "white" | "outline" | "danger";
 	fontSize?: "lg" | "md";
 	rounded?: "full" | "xl";
@@ -22,7 +20,6 @@ interface ButtonProps extends PropsWithChildren {
  *
  * @param {ButtonProps} props - 버튼 컴포넌트의 속성들.
  * @param {*} props.children - 버튼 내부에 표시될 내용.
- * @param {string} [props.className] - 버튼에 적용할 추가 커스텀 클래스 이름.
  * @param {("primary" | "secondary" | "white" | "outline" | "danger")} [props.variant="primary"] - 버튼의 종류.
  * @param {("lg" | "md")} [props.fontSize="lg"] - 버튼 텍스트의 폰트 크기.
  * @param {("full" | "xl")} [props.rounded="xl"] - 버튼의 테두리 반경.
@@ -31,37 +28,54 @@ interface ButtonProps extends PropsWithChildren {
  * @param {boolean} [props.disabled=false] - 버튼 비활성화 여부.
  * @returns {JSX.Element} 주어진 속성에 따라 스타일링된 버튼 또는 링크 엘리먼트를 반환합니다.
  */
-export default function Button({
-	children,
-	className,
-	variant = "primary",
-	fontSize = "lg",
-	rounded = "xl",
-	href,
-	onClick = () => {},
-	disabled = false,
-}: ButtonProps) {
-	const btnStyle = classNames(
-		`btn-${variant}`,
-		{
-			"rounded-full": rounded === "full",
-			"rounded-xl": rounded === "xl",
-			"text-lg": fontSize === "lg",
-			"text-md": fontSize === "md",
-		},
-		className,
-	);
+export default function Button({ children, variant = "primary", fontSize = "lg", rounded = "xl", href, onClick, disabled }: ButtonProps) {
+	let btnVariant = "";
+
+	switch (variant) {
+		case "primary":
+			btnVariant =
+				"flex items-center justify-center bg-brand-primary font-semibold text-text-inverse hover:bg-interaction-hover active:bg-interaction-pressed disabled:bg-interaction-inactive ";
+			break;
+		case "secondary":
+			btnVariant =
+				"flex items-center justify-center border border-brand-primary bg-background-inverse font-semibold text-brand-primary hover:border-interaction-hover hover:text-interaction-hover active:border-interaction-pressed active:text-interaction-pressed disabled:border-interaction-inactive disabled:text-interaction-inactive";
+			break;
+		case "white":
+			btnVariant = "flex items-center justify-center border border-text-secondary bg-background-inverse font-semibold text-text-default";
+			break;
+		case "outline":
+			btnVariant =
+				"flex items-center justify-center border border-brand-primary bg-transparent font-semibold text-brand-primary hover:border-interaction-hover hover:text-interaction-hover active:border-interaction-pressed active:text-interaction-pressed disabled:border-interaction-inactive disabled:text-interaction-inactive";
+			break;
+		case "danger":
+			btnVariant = "flex items-center justify-center bg-status-danger font-semibold text-text-inverse";
+			break;
+		default:
+			btnVariant =
+				"flex items-center justify-center bg-brand-primary font-semibold text-text-inverse hover:bg-interaction-hover active:bg-interaction-pressed disabled:bg-interaction-inactive";
+			break;
+	}
+
+	const btnStyle = `${btnVariant} ${rounded === "xl" ? "rounded-xl" : "rounded-full"} ${fontSize === "lg" ? "text-lg" : "text-md"}`;
 
 	if (href) {
 		return (
-			<Link className={btnStyle} href={href}>
+			<Link
+				className={`${btnStyle} size-full ${rounded === "xl" ? "rounded-xl" : "rounded-full"} ${fontSize === "lg" ? "py-3 text-lg" : "py-[6px] text-md"} `}
+				href={href}
+			>
 				{children}
 			</Link>
 		);
 	}
 
 	return (
-		<button type="button" className={btnStyle} onClick={onClick} disabled={disabled}>
+		<button
+			type="button"
+			className={`${btnStyle} size-full ${rounded === "xl" ? "rounded-xl" : "rounded-full"} ${fontSize === "lg" ? "py-3 text-lg" : "py-[6px] text-md"} `}
+			onClick={onClick}
+			disabled={disabled}
+		>
 			{children}
 		</button>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 /** @type {import("tailwindcss").Config} */
 export default {
-	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
 		screens: {
 			tablet: "744px",
@@ -68,4 +67,5 @@ export default {
 			xs: ["12px", "14px"],
 		},
 	},
+	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
 };
