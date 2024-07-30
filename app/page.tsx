93c269e0ea9855696ca851885a2001c9854e7ca1
jmj <wjsdncl2222@gmail.com>
2024-07-30 16:03:50 +0900

    ✨ Feat: 랜딩 페이지 반응형 디자인 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Checked from "@/public/icons/Checked";
 import Folder from "@/public/icons/Folder";
 import Mail from "@/public/icons/Mail";
-import Repair from "@/public/icons/Repair";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -16,20 +15,20 @@ export default function Page() {
 	// if(isLogin) { if(!hasTeam) { setHref("/team/create") } else { setHref("/team") } } else { setHref("/login") }
 
 	return (
-		<main className="h-full bg-background-primary">
-			<section className="flex h-dvh max-h-[1080px] flex-col justify-between">
-				<div className="absolute h-dvh max-h-[1080px] w-dvw">
-					<Image src="/images/landing_main.webp" alt="Landing main" fill priority className="object-cover object-top" />
+		<main className="h-full min-w-[330px] bg-background-primary">
+			<section className="flex h-screen flex-col items-center justify-between">
+				<div className="absolute h-screen w-full min-w-[330px]">
+					<Image src="/images/landing_main.webp" alt="Landing main" layout="fill" priority className="object-cover object-top" />
 				</div>
 
-				<div className="z-10 pt-[85px]">
-					<div className="flex items-center justify-center gap-6">
-						<p className="text-[48px] font-semibold text-text-primary">함께 만들어가는 투두 리스트</p>
-
-						<Repair width={56} height={56} />
+				<div className="z-10 pt-[85px] text-center">
+					<div className="flex items-center justify-center gap-1 tablet:gap-4 desktop:gap-6">
+						<p className="text-2xl font-semibold text-text-primary tablet:text-[40px] desktop:text-[48px]">함께 만들어가는 투두 리스트</p>
+						<div className="relative h-7 w-7 tablet:h-12 tablet:w-12 desktop:h-14 desktop:w-14">
+							<Image src="/icons/Repair.svg" alt="Repair" layout="fill" />
+						</div>
 					</div>
-
-					<p className="flex items-center justify-center bg-gradient-to-r from-brand-primary to-[#CEF57E] bg-clip-text text-[64px] font-semibold text-transparent">
+					<p className="bg-gradient-to-r from-brand-primary to-[#CEF57E] bg-clip-text text-3xl font-semibold text-transparent tablet:text-[48px] desktop:text-[64px]">
 						Coworkers
 					</p>
 				</div>
@@ -37,25 +36,26 @@ export default function Page() {
 				<div className="z-10 flex items-center justify-center pb-[180px]">
 					<Link
 						href="/login"
-						className="flex w-[370px] items-center justify-center rounded-full bg-gradient-to-r from-brand-primary to-brand-tertiary py-3 font-semibold text-text-primary"
+						className="flex w-screen max-w-[370px] items-center justify-center rounded-full bg-gradient-to-r from-brand-primary to-brand-tertiary py-3 font-semibold text-text-primary"
 					>
 						지금 시작하기
 					</Link>
 				</div>
 			</section>
 
-			<section className="flex flex-col items-center gap-20 pb-56">
+			<section className="flex flex-col items-center gap-20 px-6">
 				{/* 할 일 추가 */}
-				<div className="flex h-[419px] w-[996px] rounded-[40px] bg-conic-gradient p-[3px] shadow-linear shadow-background-inverse/25">
-					<div className="flex h-full w-full justify-center gap-48 rounded-[38px] bg-background-primary">
-						<div className="flex items-end">
-							<div className="relative h-[329px] w-[284px]">
-								<Image src="/images/landing_1.webp" alt="Landing 1" fill className="object-contain" />
+				<div className="flex h-screen max-h-[467px] w-full min-w-[330px] rounded-[40px] bg-conic-gradient p-[3px] shadow-linear shadow-background-inverse/25 tablet:max-h-[354px] tablet:max-w-[696px] desktop:max-h-[419px] desktop:max-w-[996px]">
+					<div className="flex h-full w-full flex-col-reverse items-center gap-11 rounded-[38px] bg-background-primary tablet:flex-row tablet:items-stretch tablet:justify-center tablet:gap-24 desktop:gap-48">
+						<div className="flex tablet:items-end">
+							<div className="relative h-[268px] w-[231px] desktop:h-[329px] desktop:w-[284px]">
+								<Image src="/images/landing_1.webp" alt="Landing 1" layout="fill" className="object-contain" sizes="(max-width: 284px) 100vw" />
 							</div>
 						</div>
-						<div className="flex flex-col justify-center">
+
+						<div className="flex w-[251px] flex-col tablet:w-fit tablet:justify-center">
 							<Folder width={72} height={72} />
-							<p className="pl-[12px] pt-1 text-2xl font-medium text-text-primary">
+							<p className="pl-[12px] pt-1 text-2lg font-medium text-text-primary desktop:text-2xl">
 								그룹으로 <br />할 일을 관리해요
 							</p>
 						</div>
@@ -63,43 +63,45 @@ export default function Page() {
 				</div>
 
 				{/* 멤버 초대 */}
-				<div className="flex h-[419px] w-[996px] flex-row-reverse justify-center gap-48 rounded-[40px] border border-border-primary/10 bg-background-secondary">
-					<div className="relative flex h-[329px] w-[284px] items-start">
-						<Image src="/images/landing_2.webp" alt="Landing 2" fill className="object-contain" />
+				<div className="flex h-screen max-h-[467px] w-full min-w-[330px] flex-col items-center justify-start gap-10 rounded-[40px] border border-border-primary/10 bg-background-secondary tablet:max-h-[354px] tablet:max-w-[696px] tablet:flex-row-reverse tablet:items-stretch tablet:justify-center tablet:gap-24 desktop:max-h-[419px] desktop:max-w-[996px] desktop:gap-48">
+					<div className="relative flex h-[268px] w-[231px] items-start desktop:h-[329px] desktop:w-[284px]">
+						<Image src="/images/landing_2.webp" alt="Landing 2" layout="fill" className="object-contain" sizes="(max-width: 284px) 100vw" />
 					</div>
 
-					<div className="flex flex-col items-end justify-center">
+					<div className="flex w-[251px] flex-col tablet:w-fit tablet:justify-center">
 						<Mail width={72} height={72} />
-						<p className="pr-[12px] pt-1 text-right text-2xl font-medium text-text-primary">
+						<p className="pl-[12px] pt-1 font-medium text-text-primary tablet:pl-0 tablet:pr-[12px] tablet:text-right tablet:text-2lg desktop:text-2xl">
 							간단하게 멤버들을 <br />
 							초대해요
 						</p>
 					</div>
 				</div>
 
-				<div className="flex h-[419px] w-[996px] justify-center gap-48 rounded-[40px] border border-border-primary/10 bg-[#020617]">
-					<div className="relative flex h-[329px] w-[284px] items-start">
-						<Image src="/images/landing_3.webp" alt="Landing 3" fill className="object-contain" />
+				{/* 할 일 체크 */}
+				<div className="flex h-screen max-h-[467px] w-full min-w-[330px] flex-col items-center justify-start gap-10 rounded-[40px] bg-[#020617] tablet:max-h-[354px] tablet:max-w-[696px] tablet:flex-row-reverse tablet:items-stretch tablet:justify-center tablet:gap-24 desktop:max-h-[419px] desktop:max-w-[996px] desktop:gap-48">
+					<div className="relative flex h-[268px] w-[231px] items-start desktop:h-[329px] desktop:w-[284px]">
+						<Image src="/images/landing_3.webp" alt="Landing 3" layout="fill" className="object-contain" sizes="(max-width: 284px) 100vw" />
 					</div>
 
-					<div className="flex flex-col justify-center">
+					<div className="flex w-[251px] flex-col tablet:w-fit tablet:justify-center">
 						<Checked width={72} height={72} />
-						<p className="pl-[12px] pt-1 text-2xl font-medium text-text-primary">
+						<p className="pl-[12px] pt-1 text-2lg font-medium text-text-primary desktop:text-2xl">
 							할 일도 간편하게 <br /> 체크해요
 						</p>
 					</div>
 				</div>
 			</section>
 
-			<section className="flex h-dvh max-h-[850px] flex-col justify-between">
-				<div className="absolute h-dvh max-h-[850px] w-dvw">
-					<Image src="/images/landing_footer.webp" alt="Landing footer" className="object-cover object-bottom" fill priority />
+			<section className="flex h-screen flex-col justify-between">
+				<div className="absolute h-screen w-full">
+					<Image src="/images/landing_footer.webp" alt="Landing footer" layout="fill" className="object-cover object-bottom" />
 				</div>
 
-				<div className="z-10 flex flex-col items-center justify-center gap-6">
+				<div className="z-10 flex flex-col items-center justify-center gap-6 pt-32 text-center tablet:pt-44 desktop:pt-56">
 					<p className="text-[40px] font-semibold text-text-primary">지금 바로 시작해보세요</p>
-
-					<p className="text-2xl font-medium text-text-primary">팀원 모두와 같은 방향, 같은 속도로 나아가는 가장 쉬운 방법</p>
+					<p className="text-2xl font-medium text-text-primary">
+						팀원 모두와 같은 방향, <br className="block tablet:hidden" /> 같은 속도로 나아가는 가장 쉬운 방법
+					</p>
 				</div>
 			</section>
 		</main>
