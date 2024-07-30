71899e08f75aff275fa2b5ce38aa6ce17ed464dc
jmj <wjsdncl2222@gmail.com>
2024-07-30 16:23:12 +0900

    ✨ Feat: 랜딩 페이지 layout="fill"을 fill로 수정

--- app/page.tsx
+++ app/page.tsx
@@ -18,14 +18,14 @@ export default function Page() {
 		<main className="h-full min-w-[330px] bg-background-primary">
 			<section className="flex h-screen flex-col items-center justify-between">
 				<div className="absolute h-screen w-full min-w-[330px]">
-					<Image src="/images/landing_main.webp" alt="Landing main" layout="fill" priority className="object-cover object-top" />
+					<Image src="/images/landing_main.webp" alt="Landing main" fill priority className="object-cover object-top" />
 				</div>
 
 				<div className="z-10 pt-[85px] text-center">
 					<div className="flex items-center justify-center gap-1 tablet:gap-4 desktop:gap-6">
 						<p className="text-2xl font-semibold text-text-primary tablet:text-[40px] desktop:text-[48px]">함께 만들어가는 투두 리스트</p>
 						<div className="relative h-7 w-7 tablet:h-12 tablet:w-12 desktop:h-14 desktop:w-14">
-							<Image src="/icons/Repair.svg" alt="Repair" layout="fill" />
+							<Image src="/icons/Repair.svg" alt="Repair" fill />
 						</div>
 					</div>
 					<p className="bg-gradient-to-r from-brand-primary to-[#CEF57E] bg-clip-text text-3xl font-semibold text-transparent tablet:text-[48px] desktop:text-[64px]">
@@ -49,7 +49,7 @@ export default function Page() {
 					<div className="flex h-full w-full flex-col-reverse items-center gap-11 rounded-[38px] bg-background-primary tablet:flex-row tablet:items-stretch tablet:justify-center tablet:gap-24 desktop:gap-48">
 						<div className="flex tablet:items-end">
 							<div className="relative h-[268px] w-[231px] desktop:h-[329px] desktop:w-[284px]">
-								<Image src="/images/landing_1.webp" alt="Landing 1" layout="fill" className="object-contain" sizes="(max-width: 284px) 100vw" />
+								<Image src="/images/landing_1.webp" alt="Landing 1" fill className="object-contain" sizes="(max-width: 284px) 100vw" />
 							</div>
 						</div>
 
@@ -65,7 +65,7 @@ export default function Page() {
 				{/* 멤버 초대 */}
 				<div className="flex h-screen max-h-[467px] w-full min-w-[330px] flex-col items-center justify-start gap-10 rounded-[40px] border border-border-primary/10 bg-background-secondary tablet:max-h-[354px] tablet:max-w-[696px] tablet:flex-row-reverse tablet:items-stretch tablet:justify-center tablet:gap-24 desktop:max-h-[419px] desktop:max-w-[996px] desktop:gap-48">
 					<div className="relative flex h-[268px] w-[231px] items-start desktop:h-[329px] desktop:w-[284px]">
-						<Image src="/images/landing_2.webp" alt="Landing 2" layout="fill" className="object-contain" sizes="(max-width: 284px) 100vw" />
+						<Image src="/images/landing_2.webp" alt="Landing 2" fill className="object-contain" sizes="(max-width: 284px) 100vw" />
 					</div>
 
 					<div className="flex w-[251px] flex-col tablet:w-fit tablet:justify-center">
@@ -80,7 +80,7 @@ export default function Page() {
 				{/* 할 일 체크 */}
 				<div className="flex h-screen max-h-[467px] w-full min-w-[330px] flex-col items-center justify-start gap-10 rounded-[40px] bg-[#020617] tablet:max-h-[354px] tablet:max-w-[696px] tablet:flex-row-reverse tablet:items-stretch tablet:justify-center tablet:gap-24 desktop:max-h-[419px] desktop:max-w-[996px] desktop:gap-48">
 					<div className="relative flex h-[268px] w-[231px] items-start desktop:h-[329px] desktop:w-[284px]">
-						<Image src="/images/landing_3.webp" alt="Landing 3" layout="fill" className="object-contain" sizes="(max-width: 284px) 100vw" />
+						<Image src="/images/landing_3.webp" alt="Landing 3" fill className="object-contain" sizes="(max-width: 284px) 100vw" />
 					</div>
 
 					<div className="flex w-[251px] flex-col tablet:w-fit tablet:justify-center">
@@ -94,7 +94,7 @@ export default function Page() {
 
 			<section className="flex h-screen flex-col justify-between">
 				<div className="absolute h-screen w-full">
-					<Image src="/images/landing_footer.webp" alt="Landing footer" layout="fill" className="object-cover object-bottom" />
+					<Image src="/images/landing_footer.webp" alt="Landing footer" fill className="object-cover object-bottom" />
 				</div>
 
 				<div className="z-10 flex flex-col items-center justify-center gap-6 pt-32 text-center tablet:pt-44 desktop:pt-56">
