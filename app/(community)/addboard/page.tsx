c8de4321cae3553c7fee01c180800c2bbbd75d1a
임진조 <dlawlswh123@gmail.com>
2024-08-02 16:58:55 +0900

    ✨ feat: 페이지 추가

---
+++ app/(community)/addboard/page.tsx
@@ -0,0 +1,6 @@
+export const metadata = {
+	title: "게시물 등록",
+};
+export default function AddBoardPage() {
+	return <div>게시물 등록 페이지 입니다.</div>;
+}
---
+++ app/(community)/boards/page.tsx
@@ -0,0 +1,6 @@
+export const metadata = {
+	title: "자유게시판",
+};
+export default function BoardsPage() {
+	return <div>자유게시판 페이지 입니다.</div>;
+}
