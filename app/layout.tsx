60b12f24f10d1090793a2e3efd5e0f6f671ee50a
jmj <wjsdncl2222@gmail.com>
2024-08-06 00:41:33 +0900

    ♻️ refactor : QueryProvider.tsx 이름 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 import GlobalModals from "@/app/_components/GlobalModals";
-import QueryProvider from "@/app/_components/QureyProvider";
+import QueryProvider from "@/app/_components/QueryProvider";
 
 export default function Layout({ children }: Readonly<React.PropsWithChildren>) {
 	return (
