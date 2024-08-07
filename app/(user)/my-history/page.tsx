2ce28cf2f77c034bee42e52c059828fa7e4e72dd
Sombian <sombian.software@gmail.com>
2024-08-07 22:51:44 +0900

    ✨ Feat: memo -> react-query; select

--- app/(user)/my-history/page.tsx
+++ app/(user)/my-history/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useMemo } from "react";
-
 import API from "@/app/_api";
 
 import Icon from "@/app/_icons";
@@ -11,33 +9,32 @@ import { useQuery } from "@tanstack/react-query";
 import DropDown from "@/app/_components/Dropdown";
 
 export default function Page() {
-	const { data } = useQuery({ queryKey: ["my-history"], queryFn: () => API["{teamId}/user/history"].GET({}) });
-
-	const builder = useMemo(
-		() =>
-			data?.reduce((accumulate: Record<string, (typeof data)[number]["tasksDone"]>, { tasksDone }) => {
+	const { data } = useQuery({
+		queryKey: ["my-history"],
+		queryFn: () => API["{teamId}/user/history"].GET({}),
+		select: (response) =>
+			response.reduce((accumulate: Record<string, (typeof response)[number]["tasksDone"]>, { tasksDone }) => {
 				for (const task of tasksDone) {
 					// eslint-disable-next-line no-param-reassign
 					(accumulate[task.doneAt.split("T")[0]] ??= []).push(task);
 				}
 				return accumulate;
 			}, {}),
-		[data],
-	);
+	});
 
 	return (
-		<main className="mt-[40px] flex justify-center text-text-primary">
+		<main className="flex h-full justify-center pt-[40px] text-text-primary">
 			<div className="flex w-full flex-col gap-[24px] px-[16px] desktop:container tablet:px-[24px] desktop:px-0">
 				<div className="text-xl font-bold">마이 히스토리</div>
 				{/* eslint-disable-next-line react/no-array-index-key */}
-				<div className="flex flex-col gap-[40px]">
+				<div className="flex flex-col gap-[40px] h-full">
 					{(() => {
-						if (builder && 0 < Object.keys(builder).length) {
-							return Object.keys(builder).map((key) => (
+						if (data && 0 < Object.keys(data).length) {
+							return Object.keys(data).map((key) => (
 								<div key={key} className="flex flex-col gap-[16px]">
 									<div className="text-lg font-medium">{key}</div>
 									<div className="flex flex-col gap-[16px]">
-										{builder[key].map((task) => (
+										{data[key].map((task) => (
 											<div key={task.id} className="flex h-[44px] items-center gap-[7px] rounded-[8px] bg-background-secondary px-[14px]">
 												<div className="mx-[4px] my-[4px] flex items-center justify-center rounded-[6px] bg-brand-tertiary">
 													<Icon.Check width={16} height={16} color="#FFFFFF" />
