ef2d1c5560ba5d00f8c0ee63927af65f0da3c9d3
Sombian <sombian.software@gmail.com>
2024-08-09 01:25:37 +0900

    🐛Fix: Report.tsx

--- app/(team)/[id]/_component/Report.tsx
+++ app/(team)/[id]/_component/Report.tsx
@@ -16,7 +16,7 @@ function Report() {
 	const groupId = Array.isArray(id) ? id[0] : id;
 
 	const fetchGroupInfo = useCallback((): Promise<Team> => API["{teamId}/groups/{id}"]
-		.GET({ id: groupId })
+		.GET({ id: Number(groupId) })
 		.then((response) => response)
 		.catch((error) => {
 			console.error("그룹 정보 조회 실패:", error);
