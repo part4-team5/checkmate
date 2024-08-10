1c529a0aa8d3adf9608f184a707653c2d38e35a3
YehaYoo <151544448+YehaYoo@users.noreply.github.com>
2024-08-11 00:11:55 +0900

    ✨ feat: Members 컴포넌트 구현 (#64)
    * ✨ feat: Members 컴포넌트 구현
    
    * 🐛 fix: use client 위치 변경
    
    * ♻️ refactor: 코드리뷰반영
    
    * 🔥 remove: .env 파일 삭제
    
    * ♻️ refactor: 코드리뷰반영

---
+++ app/(team)/[id]/_component/Members.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+
+import { useQuery } from "@tanstack/react-query";
+import React, { useCallback } from "react";
+import UserInfo from "@/app/(team)/[id]/_component/UserInfo";
+import API from "@/app/_api";
+import useOverlay from "@/app/_hooks/useOverlay";
+import MemberInvite from "@/app/_components/modal-contents/MemberInvite";
+import MemberProfile from "@/app/_components/modal-contents/MemberProfile";
+import { ReportProps } from "./Report";
+
+type Team = Awaited<ReturnType<(typeof API)["{teamId}/groups/{id}"]["GET"]>>;
+type Token = string;
+
+function Members({ id }: ReportProps) {
+	const overlay = useOverlay();
+
+	const fetchGroupInfo = useCallback((): Promise<Team> => API["{teamId}/groups/{id}"].GET({ id }), [id]);
+
+	const getInvitationLink = useCallback((): Promise<Token> => API["{teamId}/groups/{id}/invitation"].GET({ id }), [id]);
+
+	const { refetch } = useQuery<Token>({
+		queryKey: ["invitationLink", id],
+		queryFn: getInvitationLink,
+		enabled: false,
+	});
+
+	const { data, isLoading, error } = useQuery<Team>({
+		queryKey: ["groupInfo", id],
+		queryFn: fetchGroupInfo,
+		enabled: !!id,
+		refetchInterval: 60000,
+		retry: 3,
+		retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+	});
+
+	const handleProfileModal = useCallback(
+		(member: { userName: string; userEmail: string; userImage?: string }) => {
+			overlay.open(({ close }) => <MemberProfile userName={member.userName} email={member.userEmail} close={close} userProfile={member.userImage || ""} />);
+		},
+		[overlay],
+	);
+
+	const handleLinkCopy = async () => {
+		const { data: invitationToken } = await refetch();
+		if (invitationToken) {
+			const redirectUrl = process.env.NEXT_PUBLIC_REDIRECT_URL ?? "";
+			const params = new URLSearchParams({
+				groupId: String(id),
+				token: invitationToken,
+			});
+			const invitationUrl = `${redirectUrl}/join-team?${params.toString()}`;
+			navigator.clipboard.writeText(invitationUrl);
+		}
+	};
+
+	const handleInviteClick = () => {
+		overlay.open(({ close }) => <MemberInvite onClick={handleLinkCopy} close={close} />);
+	};
+
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>오류 발생: {error instanceof Error ? error.message : "Unknown error"}</div>;
+
+	const members = data?.members || [];
+
+	return (
+		<main className="mt-[48px]">
+			<section className="flex justify-between">
+				<div className="flex gap-[8px]">
+					<p className="text-[16px] font-medium">멤버</p>
+					<p className="text-[16px] text-[#64748B]"> ({members.length}명)</p>
+				</div>
+				<button onClick={handleInviteClick} className="text-[14px] font-normal text-brand-primary" type="button">
+					+새로운 멤버 초대하기
+				</button>
+			</section>
+			<section className="mt-[24px]">
+				<div className="grid grid-cols-2 gap-4 tablet:grid-cols-3">
+					{members.map((member) => (
+						<div
+							key={member.userId}
+							className="flex h-[68px] min-w-[164px] cursor-pointer items-center justify-start rounded-[16px] bg-background-secondary px-[16px]"
+							role="button"
+							aria-label={`${member.userName}의 프로필 열기`}
+							onClick={() => handleProfileModal(member)}
+						>
+							<UserInfo userName={member.userName} userEmail={member.userEmail} userProfile={member.userImage ?? ""} />
+						</div>
+					))}
+				</div>
+			</section>
+		</main>
+	);
+}
+
+export default Members;
--- app/_components/modal-contents/MemberInvite.tsx
+++ app/_components/modal-contents/MemberInvite.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Button from "@/app/_components/Button";
 import ModalWrapper from "@/app/_components/modal-contents/Modal";
 import CloseIcon from "@/public/icons/ic_close";
@@ -6,7 +7,19 @@ type MemberInviteProps = {
 	onClick: () => void;
 	close: () => void;
 };
+
 export default function MemberInvite({ onClick, close }: MemberInviteProps): JSX.Element {
+	const [buttonText, setButtonText] = useState("링크 복사하기");
+
+	const handleButtonClick = () => {
+		onClick();
+		setButtonText("복사됨!");
+
+		setTimeout(() => {
+			setButtonText("링크 복사하기");
+		}, 2000);
+	};
+
 	return (
 		<ModalWrapper close={close}>
 			<div className="tablet:min-w-[352px]">
@@ -20,7 +33,7 @@ export default function MemberInvite({ onClick, close }: MemberInviteProps): JSX
 						<h1 className="mt-2 text-center text-lg">멤버 초대</h1>
 						<p className="mt-2 text-center text-md">그룹에 참여할 수 있는 링크를 복사합니다.</p>
 						<div className="mt-10 h-[47px]">
-							<Button onClick={onClick}>링크 복사하기</Button>
+							<Button onClick={handleButtonClick}>{buttonText}</Button>
 						</div>
 					</div>
 				</div>
