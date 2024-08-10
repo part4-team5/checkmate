663014e3eff34a3ba530970f33ed3d3559d23fa8
jmj <wjsdncl2222@gmail.com>
2024-08-11 02:04:41 +0900

    ✨ feat : URL 토큰을 이용한 팀 가입 기능 추가

--- app/(team)/join-team/page.tsx
+++ app/(team)/join-team/page.tsx
@@ -5,7 +5,11 @@
 import API from "@/app/_api";
 import Button from "@/app/_components/Button";
 import Form from "@/app/_components/Form";
+import ModalWrapper from "@/app/_components/modal-contents/Modal";
+import useOverlay from "@/app/_hooks/useOverlay";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 type FormContext = Parameters<Parameters<typeof Form>[0]["onSubmit"]>[0];
 
@@ -13,8 +17,51 @@ export default function JoinTeam() {
 	// TODO: 유저 정보에서 이메일 가져오기 (임시 이메일)
 	const userEmail = "wwww@naver.com";
 
-	const joinTeamMutation = useMutation<{}, Error, FormContext>({
-		mutationFn: async (ctx: FormContext) => {
+	const [groupId] = useState(useSearchParams().get("groupID"));
+	const [token, setToken] = useState<string | null>(useSearchParams().get("token"));
+
+	const router = useRouter();
+
+	const overlay = useOverlay();
+
+	const openModal = useCallback(
+		(onClick: () => void, err?: string) => {
+			overlay.open(({ close }) => (
+				<ModalWrapper close={close}>
+					<div className="flex flex-col items-center justify-center gap-5 px-5 pt-5">
+						{err ? (
+							<>
+								<p className="text-2xl font-medium">팀 참여 실패</p>
+								<p className="text-2lg font-medium">{err}</p>
+							</>
+						) : (
+							<>
+								<p className="text-2xl font-medium">팀 참여 완료</p>
+								<p className="text-2lg font-medium">팀 참여가 완료되었습니다.</p>
+							</>
+						)}
+						<div className="flex h-12 w-full gap-2">
+							{!!err || <Button onClick={onClick}>팀으로 이동</Button>}
+							<Button variant="secondary" onClick={close}>
+								확인
+							</Button>
+						</div>
+					</div>
+				</ModalWrapper>
+			));
+		},
+		[overlay],
+	);
+
+	useEffect(() => {
+		if (token) {
+			// token을 숨기기 위해 주소를 변경합니다.
+			window.history.replaceState(null, "", `/join-team?groupID=${groupId}`);
+		}
+	}, [groupId, token]);
+
+	const joinTeamFormMutation = useMutation<{}, Error, FormContext>({
+		mutationFn: useCallback(async (ctx: FormContext) => {
 			const formData = new FormData();
 			for (const [key, value] of Object.entries(ctx.values)) {
 				formData.append(key, value as string);
@@ -25,21 +72,53 @@ export default function JoinTeam() {
 			const response = await API["{teamId}/groups/accept-invitation"].POST({}, { userEmail, token: teamUrl });
 
 			return response;
-		},
-		onSuccess: (data) => {
-			// TODO: 팀 참여 성공 시 팀 페이지로 이동
-			console.log("Success: ", data);
+		}, []),
+		onSuccess: () => {
+			openModal(() => {
+				router.push(`/${groupId}`);
+			});
 		},
 		onError: (error, ctx) => {
+			openModal(() => {}, error.message);
 			ctx.setError("teamUrl", "팀 링크가 올바르지 않습니다.");
 		},
 	});
 
 	const handleJoinTeam = async (ctx: FormContext) => {
-		if (joinTeamMutation.status === "pending") return;
+		if (joinTeamFormMutation.isPending) return;
 
-		joinTeamMutation.mutate(ctx);
+		joinTeamFormMutation.mutate(ctx);
 	};
+
+	const joinTeamMutation = useMutation<{}, Error, { token: string }>({
+		mutationFn: async () => {
+			const response = await API["{teamId}/groups/accept-invitation"].POST({}, { userEmail, token: token || "" });
+			return response;
+		},
+		onSuccess: () => {
+			openModal(() => {
+				router.push(`/${groupId}`);
+			});
+		},
+		onError: (error) => {
+			setToken(null);
+			openModal(() => {}, error.message);
+		},
+	});
+
+	const hasExecutedRef = useRef(false);
+
+	useEffect(() => {
+		if (token && !hasExecutedRef.current) {
+			hasExecutedRef.current = true;
+			joinTeamMutation.mutate({ token });
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	if (token) {
+		return <section className="flex size-full flex-col items-center justify-center text-3xl font-bold">팀참여 중...</section>;
+	}
 
 	return (
 		<section className="flex size-full flex-col items-center justify-center">
@@ -50,7 +129,7 @@ export default function JoinTeam() {
 			<div className="w-full">
 				<Form onSubmit={handleJoinTeam}>
 					<div className="mx-auto flex w-full max-w-[340px] flex-col tablet:max-w-[460px]">
-						<label htmlFor="teamUrl" className="w-full pb-3 text-start text-text-primary">
+						<label htmlFor="teamUrl" className="w-full pb-3 text-start text-lg text-text-primary">
 							팀 링크
 						</label>
 						<Form.Input
@@ -59,11 +138,14 @@ export default function JoinTeam() {
 							placeholder="팀 링크를 입력하세요"
 							tests={[{ type: "require", data: true, error: "팀 링크는 필수로 입력해야 합니다" }]}
 						/>
+
+						<div className="pt-3" />
+
 						<Form.Error htmlFor="teamUrl" />
 
-						<div className="pt-10" />
+						<div className="pt-6" />
 
-						<div className="h-12">{joinTeamMutation.status === "pending" ? <Button disabled>팀 참여 중</Button> : <Form.Submit>참여하기</Form.Submit>}</div>
+						<div className="h-12">{joinTeamFormMutation.isPending ? <Button disabled>팀 참여 중</Button> : <Form.Submit>참여하기</Form.Submit>}</div>
 					</div>
 				</Form>
 			</div>
