b41e18c3a752a3d1940f543a2cd5ee4aacf0008d
kyj <dbwjd338@gmail.com>
2024-08-12 00:17:01 +0900

    feat: 회원가입 페이지 구현

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -64,70 +64,68 @@ export default function LoginPage() {
 	);
 
 	return (
-		<main className="box-border min-h-screen w-full bg-background-primary p-[24px_0] tablet:p-[140px_0]">
-			<section className="m-[0_16px] w-auto tablet:m-[0_auto] tablet:w-[460px]">
-				<h2 className="mb-[80px] text-center text-[40px] font-medium leading-[48px] text-text-primary">로그인</h2>
-				<Form onSubmit={handleSubmit}>
-					<div className="flex flex-col gap-[12px]">
-						<label htmlFor="email" className="text-text-primary">
-							이메일
-						</label>
-						<Form.Input
-							id="email"
-							type="email"
-							placeholder="이메일을 입력해주세요."
-							tests={[
-								{ type: "require", data: true, error: "이메일을 입력해주세요." },
-								{ type: "match", data: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, error: "유효한 이메일이 아닙니다." },
-							]}
-						/>
-						<Form.Error htmlFor="email" />
-						<label htmlFor="password" className="mt-[12px] text-text-primary">
-							비밀번호
-						</label>
-						<Form.Input
-							id="password"
-							type="password"
-							placeholder="비밀번호를 입력해주세요."
-							tests={[
-								{
-									type: "require",
-									data: true,
-									error: "비밀번호를 입력해주세요.",
-								},
-								{
-									type: "match",
-									data: /^[a-zA-Z0-9!@#%^&*]*$/,
-									error: "비밀번호는 숫자, 영문, 특수문자만 가능합니다.",
-								},
-								{
-									type: "minlength",
-									data: 8,
-									error: "비밀번호는 최소 8자입니다.",
-								},
-								{
-									type: "maxlength",
-									data: 20,
-									error: "비밀번호는 최대 20자입니다.",
-								},
-							]}
-						/>
-						<Form.Error htmlFor="password" />
-						<Link href="/reset-password" className="text-right text-brand-primary underline">
-							비밀번호를 잊으셨나요?
-						</Link>
-					</div>
-					<div className="mb-[24px] mt-[40px] h-12">
-						<Form.Submit>로그인</Form.Submit>
-					</div>
-				</Form>
-				<div className="text-center text-text-primary">
-					처음이신가요?
-					<Link href="/signup" className="ml-[12px] text-right text-brand-primary underline">
-						가입하기
+		<>
+			<h2 className="mb-[80px] text-center text-[40px] font-medium leading-[48px] text-text-primary">로그인</h2>
+			<Form onSubmit={handleSubmit}>
+				<div className="flex flex-col gap-[12px]">
+					<label htmlFor="email" className="text-text-primary">
+						이메일
+					</label>
+					<Form.Input
+						id="email"
+						type="email"
+						placeholder="이메일을 입력해주세요."
+						tests={[
+							{ type: "require", data: true, error: "이메일을 입력해주세요." },
+							{ type: "match", data: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, error: "유효한 이메일이 아닙니다." },
+						]}
+					/>
+					<Form.Error htmlFor="email" />
+					<label htmlFor="password" className="mt-[12px] text-text-primary">
+						비밀번호
+					</label>
+					<Form.Input
+						id="password"
+						type="password"
+						placeholder="비밀번호를 입력해주세요."
+						tests={[
+							{
+								type: "require",
+								data: true,
+								error: "비밀번호를 입력해주세요.",
+							},
+							{
+								type: "match",
+								data: /^[a-zA-Z0-9!@#%^&*]*$/,
+								error: "비밀번호는 숫자, 영문, 특수문자만 가능합니다.",
+							},
+							{
+								type: "minlength",
+								data: 8,
+								error: "비밀번호는 최소 8자입니다.",
+							},
+							{
+								type: "maxlength",
+								data: 20,
+								error: "비밀번호는 최대 20자입니다.",
+							},
+						]}
+					/>
+					<Form.Error htmlFor="password" />
+					<Link href="/reset-password" className="text-right text-brand-primary underline">
+						비밀번호를 잊으셨나요?
 					</Link>
 				</div>
-			</section>
-		</main>
+				<div className="mb-[24px] mt-[40px] h-12">
+					<Form.Submit>로그인</Form.Submit>
+				</div>
+			</Form>
+			<div className="text-center text-text-primary">
+				처음이신가요?
+				<Link href="/signup" className="ml-[12px] text-right text-brand-primary underline">
+					가입하기
+				</Link>
+			</div>
+		</>
 	);
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,6 +1,156 @@
-export default function Page() {
-	return <div>WIP</div>;
-}
+"use client";
 
-/** @type {import("next").Metadata} */
-export const metadata = { title: "회원가입" };
+import Form from "@/app/_components/Form";
+import useCookie from "@/app/_hooks/useCookie";
+import API from "@/app/_api/index";
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { useCallback } from "react";
+import useAuthStore from "@/app/_store/useAuthStore";
+
+type FormContext = Parameters<Parameters<typeof Form>[0]["onSubmit"]>[0];
+
+export default function SignupPage() {
+	const router = useRouter();
+	const [accessToken, setAccessToken] = useCookie<string>("accessToken");
+	const [refreshToken, setRefreshToken] = useCookie<string>("refreshToken");
+	const setUser = useAuthStore((state) => state.setUser);
+	const user = useAuthStore((state) => state.user);
+
+	if (accessToken && refreshToken && user) {
+		router.push("/");
+	}
+
+	const signupMutation = useMutation({
+		mutationFn: async (ctx: FormContext) => {
+			const formData = new FormData();
+			Object.entries(ctx.values).forEach(([key, value]) => {
+				formData.append(key, value as string);
+			});
+
+			const email = formData.get("email") as string;
+			const nickname = formData.get("nickname") as string;
+			const password = formData.get("password") as string;
+			const passwordConfirmation = formData.get("passwordConfirmation") as string;
+
+			const payload = { email, nickname, password, passwordConfirmation };
+
+			return API["{teamId}/auth/signUp"].POST({}, payload);
+		},
+		onSuccess: (response) => {
+			setUser({
+				id: response.user.id,
+				email: response.user.email || "",
+				nickname: response.user.nickname,
+				image: response.user.image !== undefined ? response.user.image : null,
+			});
+
+			setAccessToken(response.accessToken);
+			setRefreshToken(response.refreshToken);
+			router.push("/login");
+		},
+		onError: (error) => {
+			alert("회원가입 실패");
+			console.error(error.message);
+		},
+	});
+
+	const handleSubmit = useCallback(
+		(ctx: FormContext) => {
+			if (signupMutation.status === "pending") return;
+			signupMutation.mutate(ctx);
+		},
+		[signupMutation],
+	);
+
+	return (
+		<>
+			<h2 className="mb-[80px] text-center text-[40px] font-medium leading-[48px] text-text-primary">회원가입</h2>
+			<Form onSubmit={handleSubmit}>
+				<div className="flex flex-col gap-[12px]">
+					<label htmlFor="nickname" className="text-text-primary">
+						이름
+					</label>
+					<Form.Input
+						id="nickname"
+						type="text"
+						placeholder="이름을 입력해주세요."
+						tests={[
+							{ type: "require", data: true, error: "이름을 입력해주세요." },
+							{ type: "maxlength", data: 30, error: "30자 이하로 입력해주세요." },
+						]}
+					/>
+					<Form.Error htmlFor="nickname" />
+					<label htmlFor="email" className="mt-[12px] text-text-primary">
+						이메일
+					</label>
+					<Form.Input
+						id="email"
+						type="email"
+						placeholder="이메일을 입력해주세요."
+						tests={[
+							{ type: "require", data: true, error: "이메일을 입력해주세요." },
+							{ type: "match", data: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, error: "유효한 이메일이 아닙니다." },
+						]}
+					/>
+					<Form.Error htmlFor="email" />
+					<label htmlFor="password" className="mt-[12px] text-text-primary">
+						비밀번호
+					</label>
+					<Form.Input
+						id="password"
+						type="password"
+						placeholder="비밀번호를 입력해주세요."
+						tests={[
+							{
+								type: "require",
+								data: true,
+								error: "비밀번호를 입력해주세요.",
+							},
+							{
+								type: "match",
+								data: /^[a-zA-Z0-9!@#%^&*]*$/,
+								error: "비밀번호는 숫자, 영문, 특수문자만 가능합니다.",
+							},
+							{
+								type: "minlength",
+								data: 8,
+								error: "비밀번호는 최소 8자입니다.",
+							},
+							{
+								type: "maxlength",
+								data: 20,
+								error: "비밀번호는 최대 20자입니다.",
+							},
+						]}
+					/>
+					<Form.Error htmlFor="password" />
+					<label htmlFor="passwordConfirmation" className="mt-[12px] text-text-primary">
+						비밀번호 확인
+					</label>
+					<Form.Input
+						id="passwordConfirmation"
+						type="password"
+						tests={[
+							{
+								type: "sync",
+								data: "password",
+								error: "비밀번호가 일치하지 않습니다.",
+							},
+							{
+								type: "require",
+								data: true,
+								error: "비밀번호를 입력해주세요.",
+							},
+						]}
+						placeholder="비밀번호를 다시 한 번 입력해주세요."
+					/>
+					<Form.Error htmlFor="passwordConfirmation" />
+				</div>
+				<div className="mb-[24px] mt-[40px] h-12">
+					<Form.Submit>회원가입</Form.Submit>
+				</div>
+			</Form>
+		</>
+	);
+}
