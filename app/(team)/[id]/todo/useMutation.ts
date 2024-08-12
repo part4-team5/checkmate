9a36df6575fabd7e12395243043ce89ebce772d3
임진조 <dlawlswh123@gmail.com>
2024-08-12 13:49:30 +0900

    ✨ feat: 할 일 완료에 대해 낙관적 업데이트를 위한 훅 추가

---
+++ app/(team)/[id]/todo/useMutation.ts
@@ -0,0 +1,56 @@
+import tasksKey from "@/app/(team)/[id]/todo/queryFactory";
+import API from "@/app/_api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+const PatchTodo = async (id: number, done: boolean) => {
+	const body = {
+		done,
+	};
+	const response = API["{teamId}/groups/{groupId}/task-lists/{taskListId}/tasks/{taskId}"].PATCH(
+		{
+			taskId: id,
+		},
+		body,
+	);
+	return response;
+};
+
+type TaskListType = Awaited<ReturnType<(typeof API)["{teamId}/groups/{groupId}/task-lists/{taskListId}/tasks"]["GET"]>>;
+const useTodoCheckMutation = (groupId: number, currentTaskId: number, currentDate: Date) => {
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: ({ taskId, done }: { taskId: number; done: boolean }) => PatchTodo(taskId, done),
+		onMutate: async ({ taskId, done }) => {
+			// 최신 데이터로 업데이트하기 위해 쿼리 캔슬
+			await queryClient.cancelQueries({ queryKey: tasksKey.detail(groupId, currentTaskId, currentDate.toLocaleDateString("ko-KR")) });
+			// 이전 데이터를 저장
+			const oldData = queryClient.getQueryData<TaskListType>(tasksKey.detail(groupId, currentTaskId, currentDate.toLocaleDateString("ko-KR")));
+			// 새로운 데이터로 업데이트
+			const newData = oldData?.map((todo) => {
+				if (todo.id === taskId) {
+					return { ...todo, doneAt: done ? new Date().toISOString() : null } as TaskListType[0];
+				}
+				return todo; // 기존 값 반환
+			});
+
+			queryClient.setQueryData<TaskListType>(tasksKey.detail(groupId, currentTaskId, currentDate.toLocaleDateString("ko-KR")), newData);
+
+			// 이전 데이터를 반환
+			return { oldData };
+		},
+		// 요청 실패 시 이전 데이터로 롤백
+		onError: (error, variables, context) => {
+			if (context?.oldData) {
+				queryClient.setQueryData(tasksKey.detail(groupId, currentTaskId, currentDate.toLocaleDateString("ko-KR")), context.oldData);
+			}
+			alert(`오류: ${error.message} - ${variables.done ? "완료" : "미완료"} 처리에 실패했습니다.`);
+		},
+		// 요청이 성공하던 실패하던 무효화해서 최신 데이터로 업데이트
+		onSettled: () => {
+			queryClient.invalidateQueries({ queryKey: tasksKey.detail(groupId, currentTaskId, currentDate.toLocaleDateString("ko-KR")) });
+			queryClient.invalidateQueries({ queryKey: ["tasks", { taskId: currentTaskId }] });
+		},
+	});
+};
+
+export default useTodoCheckMutation;
