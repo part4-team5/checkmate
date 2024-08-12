ed01bb15f5294900fcbddcf8cdc7a814b101a1fb
임진조 <dlawlswh123@gmail.com>
2024-08-12 11:37:03 +0900

    💄  ui: 댓글 제출 버튼 + 기본 프로필 이미지 추가

--- app/_icons/index.tsx
+++ app/_icons/index.tsx
@@ -164,3 +164,11 @@ Icon.TodoCheck = function TodoCheck({ width = 16, height = 16, color = "none" }:
 		</svg>
 	);
 };
+
+Icon.CheckButton = function CheckButton({ width, height, color = "#F8FAFC" }: IconProps) {
+	return (
+		<svg width={width} height={height} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M4 7.14286L6.90909 10L12 5" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+		</svg>
+	);
+};
