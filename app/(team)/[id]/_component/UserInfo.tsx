d5298c72b400fa0b93d2d0ae18fdeebeb7cb16a0
YehaYoo <151544448+YehaYoo@users.noreply.github.com>
2024-08-22 11:18:53 +0900

    ♻️ refactor : 팀 페이지 개선 사항 반영 (#95)
    * ♻️ refactor : 개선 사항 반영
    
    * 💄 style: 스켈레톤 수정
    
    * 🐛 fix: 관리자 아이콘 표시 오류 수정
    
    * 🐛 refactor: 개선 사항 반영

--- app/(team)/[id]/_component/UserInfo.tsx
+++ app/(team)/[id]/_component/UserInfo.tsx
@@ -2,25 +2,31 @@
 
 import Image from "next/image";
 import defaultImage from "@/public/icons/defaultAvatar.svg";
+import Icon from "@/app/_icons";
 
 interface UserInfoProps {
 	userProfile?: string;
 	userName: string;
 	userEmail?: string;
+	isAdmin?: boolean;
 }
 
-export default function UserInfo({ userProfile, userName, userEmail }: UserInfoProps) {
+export default function UserInfo({ userProfile, userName, userEmail, isAdmin }: UserInfoProps) {
 	return (
 		<div className="flex items-center gap-[12px]">
 			<div className="hidden tablet:block">
 				<Image src={userProfile || defaultImage} alt={userName} width={32} height={32} />
 			</div>
 			<div className="flex flex-col gap-[6px]">
-				<div className="flex items-center justify-start gap-[8px] tablet:hidden">
+				<div className="flex items-center justify-start gap-[6px] tablet:hidden">
 					<Image src={userProfile || defaultImage} alt={userName} width={32} height={32} />
 					<div className="text-primary text-md font-medium">{userName}</div>
+					{isAdmin && <Icon.Crown width={11} height={11} />}
 				</div>
-				<div className="text-primary hidden text-md font-medium tablet:block">{userName}</div>
+				<div className="text-primary hidden items-center gap-[6px] text-md font-medium tablet:flex">
+					{userName}
+					{isAdmin && <Icon.Crown width={11} height={11} />}
+				</div>
 				{userEmail && <div className="text-secondary text-xs">{userEmail}</div>}
 			</div>
 		</div>
