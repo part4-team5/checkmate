6d578263c718465f63383739f9c3d07b63949b65
jmj <wjsdncl2222@gmail.com>
2024-08-23 23:04:42 +0900

    💄 style : 토스트 아이콘 수정 및 크기 수정

--- app/_components/Toaster.tsx
+++ app/_components/Toaster.tsx
@@ -15,21 +15,6 @@ function Toaster() {
 		clearToasts();
 	}, [pathname, clearToasts]);
 
-	const style = (type: string) => {
-		switch (type) {
-			case "info":
-				return "bg-white text-black";
-			case "success":
-				return "bg-white text-black";
-			case "error":
-				return "bg-white text-black";
-			case "warning":
-				return "bg-white text-black";
-			default:
-				return "bg-white text-black";
-		}
-	};
-
 	const animation = (type: string) => {
 		switch (type) {
 			case "error":
@@ -63,9 +48,17 @@ function Toaster() {
 	const icon = (type: string) => {
 		switch (type) {
 			case "info":
-				return <Icon.Info width={24} height={24} color="#3B82F6" />;
+				return (
+					<div className="w-6">
+						<Icon.Info width={24} height={24} color="#3B82F6" />
+					</div>
+				);
 			case "success":
-				return <Icon.CheckAnimation />;
+				return (
+					<div className="size-6 pr-5">
+						<Icon.CheckAnimation />
+					</div>
+				);
 			case "error":
 				return (
 					<div className="rounded-full bg-status-danger p-1">
@@ -86,7 +79,7 @@ function Toaster() {
 	};
 
 	return (
-		<div className="fixed right-1/2 top-10 z-50 flex translate-x-1/2 flex-col items-center justify-center space-y-2 transition-all">
+		<div className="fixed right-1/2 top-10 z-50 flex w-max translate-x-1/2 flex-col items-center justify-center space-y-2 transition-all">
 			<AnimatePresence>
 				{toasts.map((toast) => (
 					<motion.div
@@ -96,7 +89,7 @@ function Toaster() {
 						exit={animation(toast.type).exit}
 						transition={{ duration: 0.3 }}
 						onClick={() => removeToast(toast.id)}
-						className={`w-fit cursor-pointer justify-end rounded-lg p-4 text-lg font-medium shadow-lg ${style(toast.type)}`}
+						className="w-fit cursor-pointer justify-end rounded-lg bg-white p-4 text-lg font-medium text-black shadow-lg"
 					>
 						<div className="flex items-center justify-between gap-2">
 							{icon(toast.type)}
