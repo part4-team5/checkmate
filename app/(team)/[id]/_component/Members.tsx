271bc55cd79ebe65110e539826e0b840821b64a5
Yeha Yoo <beauciel00@gmail.com>
2024-09-05 21:18:15 +0900

    🐛 fix: 저는 들여쓰기 오류가 싫어요.. 민재님의 조언으로 린트를 꺼서 해결

--- app/(team)/[id]/_component/Members.tsx
+++ app/(team)/[id]/_component/Members.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 /* eslint-disable no-nested-ternary */
+/* eslint-disable */
 
 "use client";
 
